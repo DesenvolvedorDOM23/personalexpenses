e6f6fe5e793e9e564691737f6793f5e9ae807299
DesenvolvedorDOM23 <elesouzadom@gmail.com>
2023-05-31 14:08:20 -0400

    crei os components chart e chartbar

---
+++ lib/widget/components/chart.dart
@@ -0,0 +1,61 @@
+import 'package:expenses/widget/components/chart_appbar.dart';
+import 'package:flutter/material.dart';
+import '../../model/trasaction_model.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+  const Chart(this.recentTransaction);
+
+  List<Map<String, Object?>> get groupedTransaction {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalsum = 0.0;
+
+      for (var i = 0; i < recentTransaction.length; i++) {
+        bool sameDay = recentTransaction[i].date.day == weekDay.day;
+        bool sameMonth = recentTransaction[i].date.month == weekDay.month;
+        bool sameYar = recentTransaction[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYar) {
+          totalsum += recentTransaction[i].value;
+        }
+      }
+
+      return {'day': DateFormat.E().format(weekDay)[0], 'value': totalsum};
+    });
+  }
+
+  double get _weekTotalValue {
+    return groupedTransaction.fold(0.0, (sum, tr) {
+      return sum + (tr['value'] as double);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.all(20),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransaction.map((tr) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: Chartbar(
+                label: tr['day'].toString(),
+                value: tr['value'] as double,
+                percentage: (tr['value'] as double) / _weekTotalValue,
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
--- lib/widget/widget_app.dart
+++ lib/widget/widget_app.dart
@@ -4,32 +4,30 @@ import 'home.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final tema = ThemeData();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: tema.copyWith(
-          colorScheme: tema.colorScheme.copyWith(
-            primary: Colors.purple,
-            secondary: Colors.amber,
-          ),
+      title: 'Depesas Pesoais',
+      theme: ThemeData(
+          primaryColor: Colors.purple,
+          fontFamily: 'Quicksand',
           textTheme: tema.textTheme.copyWith(
             titleLarge: const TextStyle(
               fontFamily: 'OpenSans',
-              fontSize: 18,
+              fontSize: 20,
               fontWeight: FontWeight.bold,
               color: Colors.black,
             ),
           ),
           appBarTheme: const AppBarTheme(
-            titleTextStyle: TextStyle(
-                fontFamily: 'OpenSans',
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
-          )),
+            color: Colors.purple,
+            titleTextStyle:
+                TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: Colors.amber[900])),
       home: const HomePage(),
     );
   }
